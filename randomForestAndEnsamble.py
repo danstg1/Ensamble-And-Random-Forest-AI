2532855b61298689d652987671d6a0eb2d530a32
danstg1 <104002083+danstg1@users.noreply.github.com>
2023-09-28 15:20:32 +0100

    Add files via upload

---
+++ randomForestAndEnsamble.py
@@ -0,0 +1,148 @@
+import sys
+from packaging import version
+import sklearn
+import matplotlib.pyplot as plt
+
+plt.rc('font', size=14)
+plt.rc('axes', labelsize=14, titlesize=14)
+plt.rc('legend', fontsize=14)
+plt.rc('xtick', labelsize=10)
+plt.rc('ytick', labelsize=10)
+
+from pathlib import Path
+
+IMAGES_PATH = Path() / "images" / "ensembles"
+IMAGES_PATH.mkdir(parents=True, exist_ok=True)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = IMAGES_PATH / f"{fig_id}.{fig_extension}"
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+
+"""
+Building voting classifier
+"""
+
+from sklearn.datasets import make_moons
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+
+X, y = make_moons(n_samples=500, noise=0.30, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+voting_clf = VotingClassifier(
+    estimators=[
+        ('lr', LogisticRegression(random_state=42)),
+        ('rf', RandomForestClassifier(random_state=42)),
+        ('svc', SVC(random_state=42))
+    ]
+)
+voting_clf.fit(X_train, y_train)
+
+"""
+Using Soft Voting
+"""
+
+voting_clf.voting = "soft"
+voting_clf.named_estimators["svc"].probability = True
+voting_clf.fit(X_train, y_train)
+voting_clf.score(X_test, y_test)
+
+
+"""
+Bagging example
+"""
+
+from sklearn.ensemble import BaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+bag_clf = BaggingClassifier(DecisionTreeClassifier(), n_estimators=500,
+                            max_samples=100, n_jobs=-1, random_state=42)
+bag_clf.fit(X_train, y_train)
+
+"""
+OOB
+"""
+
+bag_clf = BaggingClassifier(DecisionTreeClassifier(), n_estimators=500,
+                            oob_score=True, n_jobs=-1, random_state=42)
+bag_clf.fit(X_train, y_train)
+bag_clf.oob_score_
+
+"""
+Random Forrest
+"""
+from sklearn.ensemble import RandomForestClassifier
+
+rnd_clf = RandomForestClassifier(n_estimators=500, max_leaf_nodes=16,
+                                 n_jobs=-1, random_state=42)
+rnd_clf.fit(X_train, y_train)
+y_pred_rf = rnd_clf.predict(X_test)
+
+bag_clf = BaggingClassifier(
+    DecisionTreeClassifier(max_features="sqrt", max_leaf_nodes=16),
+    n_estimators=500, n_jobs=-1, random_state=42)
+
+
+"""
+Get Feature Importance
+"""
+
+from sklearn.datasets import load_iris
+
+iris = load_iris(as_frame=True)
+rnd_clf = RandomForestClassifier(n_estimators=500, random_state=42)
+rnd_clf.fit(iris.data, iris.target)
+for score, name in zip(rnd_clf.feature_importances_, iris.data.columns):
+    print(round(score, 2), name)
+
+
+"""
+ADA BOOSTING
+"""
+
+from sklearn.ensemble import AdaBoostClassifier
+
+ada_clf = AdaBoostClassifier(
+    DecisionTreeClassifier(max_depth=1), n_estimators=30,
+    learning_rate=0.5, random_state=42)
+ada_clf.fit(X_train, y_train)
+
+
+"""
+Gradient Boosting
+"""
+
+import numpy as np
+from sklearn.tree import DecisionTreeRegressor
+
+np.random.seed(42)
+X = np.random.rand(100, 1) - 0.5
+y = 3 * X[:, 0] ** 2 + 0.05 * np.random.randn(100)  # y = 3x² + Gaussian noise
+
+tree_reg1 = DecisionTreeRegressor(max_depth=2, random_state=42)
+tree_reg1.fit(X, y)
+
+
+"""
+Stacking
+"""
+
+from sklearn.ensemble import StackingClassifier
+
+stacking_clf = StackingClassifier(
+    estimators=[
+        ('lr', LogisticRegression(random_state=42)),
+        ('rf', RandomForestClassifier(random_state=42)),
+        ('svc', SVC(probability=True, random_state=42))
+    ],
+    final_estimator=RandomForestClassifier(random_state=43),
+    cv=5  # number of cross-validation folds
+)
+stacking_clf.fit(X_train, y_train)
+
+stacking_clf.score(X_test, y_test)
